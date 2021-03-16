c35a71c1d46380606374acdf77205036907c5fa3
hugo navillod <hugo.navillod@edu.ece.fr>
2021-03-16 18:28:37 +0100

    Ajout des variables dans enregistrement + commencement des onclickisteners

--- app/src/main/java/fr/android/moi/tennistracker/Enregistrement.java
+++ app/src/main/java/fr/android/moi/tennistracker/Enregistrement.java
@@ -1,64 +1,62 @@
 package fr.android.moi.tennistracker;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Enregistrement#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.Console;
+
 public class Enregistrement extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Enregistrement() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Enregistrement.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Enregistrement newInstance(String param1, String param2) {
-        Enregistrement fragment = new Enregistrement();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_enregistrement, container, false);
+
+        //TextView J1
+        TextView pointActuelJ1 = view.findViewById(R.id.pointActuelJ1);
+        TextView set1J1 = view.findViewById(R.id.set1J1);
+        TextView set2J1 = view.findViewById(R.id.set2J1);
+
+        //TextView J2
+        TextView pointActuelJ2 = view.findViewById(R.id.pointActuelJ2);
+        TextView set1J2 = view.findViewById(R.id.set1J2);
+        TextView set2J2 = view.findViewById(R.id.set2J2);
+
+        //Buttons
+        Button premiereOK = view.findViewById(R.id.premiereOK);
+        Button deuxiemeOK = view.findViewById(R.id.deuxiemeOK);
+        Button ace1 = view.findViewById(R.id.ace1);
+        Button ace2 = view.findViewById(R.id.ace2);
+        Button doubleFaute = view.findViewById(R.id.doubleFaute);
+        Button pointGagnantJ1 = view.findViewById(R.id.pointGagnantJ1);
+        Button fauteProvoquéeJ1 = view.findViewById(R.id.fauteProvoquéeJ1);
+        Button fauteDirecteJ1 = view.findViewById(R.id.fauteDirecteJ1);
+        Button pointGagnantJ2 = view.findViewById(R.id.pointGagnantJ2);
+        Button fauteProvoquéeJ2 = view.findViewById(R.id.fauteProvoquéeJ2);
+        Button fauteDirecteJ2 = view.findViewById(R.id.fauteDirecteJ2);
+
+        //Button ace 1
+        ace1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pointActuelJ1.setText("15");
+            }
+        });
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_enregistrement, container, false);
-    }
+
 }
